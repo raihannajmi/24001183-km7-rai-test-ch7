d354824f09add6d08314b5948611ab6bf68e55c7
ImamTaufiqHermawan <imamtaufiq133@gmail.com>
2024-04-13 07:29:17 +0700

    add script for running sequelize command

--- config/database.js
+++ config/database.js
@@ -7,8 +7,8 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST || "localhost",
     dialect: "postgres",
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT || 5432,
   },
 };
