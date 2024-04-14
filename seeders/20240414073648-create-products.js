6d23e1b782800b3f0f188e5f61f39734c2029693
ImamTaufiqHermawan <imamtaufiq133@gmail.com>
2024-04-14 14:55:22 +0700

    fix error in seeding products

--- seeders/20240414073648-create-products.js
+++ seeders/20240414073648-create-products.js
@@ -2,9 +2,7 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert("Products", []);
-  },
+  async up(queryInterface, Sequelize) {},
 
   async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete("Products", null, {});
